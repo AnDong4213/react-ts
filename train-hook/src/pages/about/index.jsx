8b867b52d140e50c5d33c48213771794a46f22a1
赵安东 <zhaoandong4213@sina.com>
2021-07-29 10:24:17 +0800

    Hook

--- train-hook/src/pages/about/index.jsx
+++ train-hook/src/pages/about/index.jsx
@@ -1,18 +1,9 @@
 import { useEffect, useState, useMemo } from "react";
 
 const About = () => {
-  const [count, setCount] = useState(0); // 加了这一行
-  // const value = { name: 1 };
-  const value = useMemo(() => ({ name: 1 }), []);
-
-  useEffect(() => {
-    setCount(Math.random());
-    console.log("render");
-  }, [value]);
-
   return (
     <div className="App">
-      <h1>Hello CodeSandbox{count}</h1>
+      <h1>Hello CodeSandbox</h1>
       <h2>Edit to see some magic happen!</h2>
     </div>
   );
