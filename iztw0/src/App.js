19f65f8b1a72ba293116a8cd3e4294564debe761
赵安东 <zhaoandong4213@sina.com>
2022-01-04 18:49:59 +0800

    切片

--- iztw0/src/App.js
+++ iztw0/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
 
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Counter />
       </header>
     </div>
--- iztw0/src/features/counter/Counter.js
+++ iztw0/src/features/counter/Counter.js
@@ -1,27 +1,24 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
   decrement,
   increment,
   incrementByAmount,
   incrementAsync,
-  selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+  selectCount
+} from "./counterSlice";
+import styles from "./Counter.module.css";
+// console.log(styles);
 
 export function Counter() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const [incrementAmount, setIncrementAmount] = useState("2");
 
   return (
     <div>
       <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
+        <button className={styles.button} onClick={() => dispatch(increment())}>
           +
         </button>
         <span className={styles.value}>{count}</span>
@@ -33,12 +30,12 @@ export function Counter() {
           -
         </button>
       </div>
+      <div className={styles.row}>.row:not(:last-child)</div>
       <div className={styles.row}>
         <input
           className={styles.textbox}
-          aria-label="Set increment amount"
           value={incrementAmount}
-          onChange={e => setIncrementAmount(e.target.value)}
+          onChange={(e) => setIncrementAmount(e.target.value)}
         />
         <button
           className={styles.button}
