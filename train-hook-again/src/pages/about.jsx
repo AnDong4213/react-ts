31ddd9af8e3a6de03952204bec1eb677f95bb12e
赵安东 <zhaoandong4213@sina.com>
2022-02-23 11:24:54 +0800

    fix

--- train-hook-again/src/pages/about.jsx
+++ train-hook-again/src/pages/about.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
+import logo from '../assets/logo.png'
 
-export default function about() {
+export default function About() {
+const [test, setTest] = useState(10)
+
   return (
-    <div>about</div>
+    <section>
+        <p>about</p>
+        <p>{test}</p>
+        <div><img src={logo} alt='' /><h3>我是图片</h3></div>
+    </section>
   )
 }
--- train-hook-again/src/pages/home/index.jsx
+++ train-hook-again/src/pages/home/index.jsx
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class Home extends Component {
+  state = {
+    age: 7
+  }
+  
   render() {
     return <div>Home</div>;
   }
