639ae39f0f5884aade7df7bc2a0a52f03fb6ae40
赵安东 <zhaoandong4213@sina.com>
2022-03-31 16:26:35 +0800

    fix: 删除APP2

--- myapp/src/models/counter.ts
+++ myapp/src/models/counter.ts
@@ -1,7 +1,7 @@
 import { useState, useCallback } from 'react';
 
 export default () => {
-    const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState(10);
     const increment = useCallback(() => setCounter((c) => c + 1), []);
     const decrement = useCallback(() => setCounter((c) => c - 1), []);
     return { counter, increment, decrement };
