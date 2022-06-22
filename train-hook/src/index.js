9d3f52b653f5e83e978d09db51b9c6b28dba67e9
赵安东 <zhaoandong4213@sina.com>
2022-06-22 10:13:06 +0800

    feat: TreeSelect

--- train-hook/src/index.js
+++ train-hook/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import "./index.css";
+import "antd/dist/antd.css";
 import App from "./App.jsx";
 import store from "./store";
 import reportWebVitals from "./reportWebVitals";
